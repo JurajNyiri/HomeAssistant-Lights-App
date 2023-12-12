9a32f6a2609178d16f3e675d31ceb736817ccb76
Juraj Nyíri <juraj.nyiri@gmail.com>
2023-12-12 17:34:31 +0100

    Update: Simplify/Refactor connecting

--- custom_components/lights_app/__init__.py
+++ custom_components/lights_app/__init__.py
@@ -16,6 +16,56 @@ from .utils import (
 )
 
 
+async def setupConnection(hass, address, config_entry):
+    if not hass.data[DOMAIN][config_entry.entry_id]["connection"]["connecting"]:
+        hass.data[DOMAIN][config_entry.entry_id]["connection"]["connecting"] = True
+        ble_device = bluetooth.async_ble_device_from_address(
+            hass, address, connectable=True
+        )
+        if ble_device:
+            LOGGER.warn("BLE Device found, connecting...")
+            hass.data[DOMAIN][config_entry.entry_id]["connection"][
+                "client"
+            ] = await establish_connection(
+                BleakClientWithServiceCache,
+                ble_device,
+                name=address,
+                disconnected_callback=disconnect_handler(
+                    hass.data[DOMAIN][config_entry.entry_id]
+                ),
+                use_services_cache=True,
+                max_attempts=1,
+            )
+            hass.data[DOMAIN][config_entry.entry_id]["connection"]["connected"] = True
+            LOGGER.warn(
+                "Connected successfully, setting up subscribers and getting data..."
+            )
+            client = hass.data[DOMAIN][config_entry.entry_id]["connection"]["client"]
+            hass.data[DOMAIN][config_entry.entry_id]["connection"][
+                "service"
+            ] = client.services.get_service(SERVICE)
+            communicationService = hass.data[DOMAIN][config_entry.entry_id][
+                "connection"
+            ]["service"]
+
+            if communicationService:
+                await client.start_notify(
+                    getNotifyCharacteristic(communicationService),
+                    notification_handler(hass.data[DOMAIN][config_entry.entry_id]),
+                )
+
+                await sendCommand(
+                    client,
+                    communicationService,
+                    getLightStateCommand(),
+                )
+
+            LOGGER.warn("Connection completed.")
+        else:
+            LOGGER.warn("BLE Device not found.")
+        hass.data[DOMAIN][config_entry.entry_id]["connection"]["connecting"] = False
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up the Lights App component from a config entry."""
     hass.data.setdefault(DOMAIN, {})
@@ -31,56 +81,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
             "entities": [],
             "state": None,
             "statePending": True,
-            "connection": {},
+            "connection": {"connected": False, "connecting": False},
         }
 
         async def async_update_data():
             LOGGER.warn("async_update_data - entry")
             if not hass.data[DOMAIN][entry.entry_id]["connection"]["connected"]:
                 LOGGER.warn("Not connected, reconnecting...")
-                ble_device = bluetooth.async_ble_device_from_address(
-                    hass, address, connectable=True
-                )
-                if ble_device:
-                    LOGGER.warn("BLE Device found, connecting...")
-                    hass.data[DOMAIN][entry.entry_id]["connection"][
-                        "client"
-                    ] = await establish_connection(
-                        BleakClientWithServiceCache,
-                        ble_device,
-                        name=address,
-                        disconnected_callback=disconnect_handler(
-                            hass.data[DOMAIN][entry.entry_id]
-                        ),
-                        use_services_cache=True,
-                        max_attempts=1,
-                    )
-                    LOGGER.warn(
-                        "Reconnected successfully, setting up subscribers and getting data..."
-                    )
-                    client = hass.data[DOMAIN][entry.entry_id]["connection"]["client"]
-                    hass.data[DOMAIN][entry.entry_id]["connection"][
-                        "service"
-                    ] = client.services.get_service(SERVICE)
-                    communicationService = hass.data[DOMAIN][entry.entry_id][
-                        "connection"
-                    ]["service"]
-
-                    if communicationService:
-                        await client.start_notify(
-                            getNotifyCharacteristic(communicationService),
-                            notification_handler(hass.data[DOMAIN][entry.entry_id]),
-                        )
-
-                        await sendCommand(
-                            client,
-                            communicationService,
-                            getLightStateCommand(),
-                        )
-
-                    LOGGER.warn("Reconnect completed.")
-                else:
-                    LOGGER.warn("BLE Device not found.")
+                await setupConnection(hass, address, entry)
 
         lightsAppCoordinator = DataUpdateCoordinator(
             hass,
@@ -90,40 +98,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         )
         hass.data[DOMAIN][entry.entry_id]["coordinator"] = lightsAppCoordinator
 
-        # todo what happens if connection fails?
-        hass.data[DOMAIN][entry.entry_id]["connection"][
-            "client"
-        ] = await establish_connection(
-            BleakClientWithServiceCache,
-            ble_device,
-            address,
-            disconnect_handler(hass.data[DOMAIN][entry.entry_id]),
-            use_services_cache=True,
+        await setupConnection(hass, address, entry)
+
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, "light")
         )
-        hass.data[DOMAIN][entry.entry_id]["connection"]["connected"] = True
-        # todo add refresh that detects whether we are connected or not
 
-        client = hass.data[DOMAIN][entry.entry_id]["connection"]["client"]
-        hass.data[DOMAIN][entry.entry_id]["connection"][
-            "service"
-        ] = client.services.get_service(SERVICE)
-        communicationService = hass.data[DOMAIN][entry.entry_id]["connection"][
-            "service"
-        ]
-        if communicationService:
-            await client.start_notify(
-                getNotifyCharacteristic(communicationService),
-                notification_handler(hass.data[DOMAIN][entry.entry_id]),
-            )
-
-            await sendCommand(
-                client,
-                communicationService,
-                getLightStateCommand(),
-            )
-
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(entry, "light")
-            )
         return True
     raise ConfigEntryNotReady()
